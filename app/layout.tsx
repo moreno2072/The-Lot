d2e34aa7d48f6ef26573e45cfc8243a542e57587
moreno2072 <347moreno20722@gmail.com>
2026-09-03 12:08:16 -0400

    Await session retrieval in RootLayout component

--- app/layout.tsx
+++ app/layout.tsx
@@ -9,7 +9,7 @@ export const metadata: Metadata = {
 };
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
-  const session = getSession();
+   const session = await getSession(); 
   return (
     <html lang="en">
       <body className="font-body min-h-screen">
